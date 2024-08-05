87388a50c3dbf36914ea6cdd812056bd16bed71a
salk50 <65875950+salk50@users.noreply.github.com>
2024-08-06 01:25:44 +0400

    despite some bugs, added basic 2-number calculator operations

--- js/script.js
+++ js/script.js
@@ -15,7 +15,98 @@ function divi(a,b) {
 };
 
 btn = document.querySelectorAll("#num");
+disres = document.querySelector(".one");
+disop = document.querySelector(".two");
+oper = document.querySelectorAll("#opr");
+fin = document.querySelector('#fin');
+clear = document.querySelector('#backspace');
+reset = document.querySelector('#reset');
 
-btn.forEach(key => key.addEventListener('click', (e)=>{
+function numbers() {
+    console.log(this.textContent);
+    if (disop.textContent === "0" && this.textContent === "0") {
+        disop.textContent = this.textContent;
+    }
+    else if (disop.textContent === "0" && this.textContent !== "0") {
+        disop.textContent = this.textContent;
+    }
+    else {disop.textContent += this.textContent;};
+}
+
+
+function operator() {
+    console.log(this.textContent);
+    if(disop.textContent.split(' ').length <= 3) {
+        if(disop.textContent.at(-1) == ' ') {
+            let myr = disop.textContent.split(' ');
+            console.log(myr)
+            myr[myr.length - 2]=`${this.textContent}`;
+            disop.textContent = myr.join(' ');
+        }
+        else if(typeof parseInt(disop.textContent) === "number" && typeof disop.textContent.at(-1) !== ' ') {
+            disop.textContent += ` ${this.textContent} `;
+        };
+    }
+    else if(disop.textContent.split(' ').length <= 1) {
+        disop.textContent = disres.textContent;
+        disop.textContent += ` ${this.textContent} `;
+    }
+    else {
+        finish();
+    };
+};
+
+let marray = [];
+function finish(){
+    marray = disop.textContent.split(' ');
+    let sum = 0;
+    console.log(sum);
+    console.log(marray);
+    if(marray.length === 3) {
+        let a = parseFloat(marray[0]);
+        let op = marray[1];
+        let b = parseFloat(marray[2]);
+        console.log(a);console.log(b);console.log(op);
+        switch (op) {
+            case '+':
+                sum += add(a,b);
+                break;
+            case '-':
+                sum += sub(a,b);
+                break;
+            case '*':
+                sum += mult(a,b);
+                break;
+            case '/':
+                sum += divi(a,b);
+                break;
+            default:
+                break;
+        }
+        disres.textContent = sum;
+        disop.textContent = sum;
+    }
+    else if(isNaN(parseFloat(disres.textContent))) {
+        disres.textContent = '0';
+        disop.textContent = disres.textContent;
+    }
+    else {
+        disres.textContent = disop.textContent;
+    }
+};
+
+btn.forEach(key => key.addEventListener("click", numbers));
+oper.forEach(key => key.addEventListener("click", operator));
+fin.addEventListener("click", finish);
+clear.addEventListener("click", (e) => {
     console.log(e.target.textContent);
-}));
+    let myr = disop.textContent.split(' ');
+    myr[myr.length - 1] = '';
+    disop.textContent = myr.join(' ');
+});
+
+reset.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+    disres.textContent = 'This line will be over-written by the result';
+    disop.textContent = 0;
+});
